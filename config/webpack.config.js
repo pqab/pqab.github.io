0af8ad9f8b45d5bb8748806f840d8a56335d1196
pqab <pqab.annie@gmail.com>
2022-12-01 21:53:51 +0900

    Build production

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -203,19 +203,19 @@ module.exports = function (webpackEnv) {
     entry: paths.appIndexJs,
     output: {
       // The build folder.
-      path: paths.appBuild,
+      path: paths.appPath,
       // Add /* filename */ comments to generated require()s in the output.
       pathinfo: isEnvDevelopment,
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].js'
-        : isEnvDevelopment && 'static/js/bundle.js',
+        ? 'build/static/js/[name].[contenthash:8].js'
+        : isEnvDevelopment && 'build/static/js/bundle.js',
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].chunk.js'
-        : isEnvDevelopment && 'static/js/[name].chunk.js',
-      assetModuleFilename: 'static/media/[name].[hash][ext]',
+        ? 'build/static/js/[name].[contenthash:8].chunk.js'
+        : isEnvDevelopment && 'build/static/js/[name].chunk.js',
+      assetModuleFilename: 'build/static/media/[name].[hash][ext]',
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
